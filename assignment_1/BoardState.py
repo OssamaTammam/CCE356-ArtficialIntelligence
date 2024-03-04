d8a52ff1c0029ac8477f533be978aae6cd7ec0ef
Ossama Tammam <ossamatammam02@gmail.com>
2024-03-04 23:41:09 +0200

    bug fix: neighbors are now calculated on demand

--- assignment_1/BoardState.py
+++ assignment_1/BoardState.py
@@ -4,16 +4,24 @@ import random
 class BoardState:
     goalState = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
-    def __init__(self, prevBoardState=None):
+    def __init__(self, state: list = None, boardState: "BoardState" = None):
+        # check if it's already an object and clone it
+        if boardState is not None:
+            self.state = boardState.state.copy()
+            self.neighbors = boardState.neighbors.copy()
+            return
+
+        # check if state is present
+        if state is not None:
+            self.state = state
+
         # if state is not specified generate a random state
-        if prevBoardState.state is None:
+        if state is None and boardState is None:
             self.state = list(range(9))
             random.shuffle(self.state)
-        else:
-            self.state = prevBoardState.state
 
-        self.neighboringStates = []
-        self.calcNeighbors()
+        # calculate the neighboring states for each board
+        self.neighbors = []
 
     def calcNeighbors(self):
         possibleMoves = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # (row, col)
@@ -24,17 +32,19 @@ class BoardState:
             newRow, newCol = currRow + rowMove, currCol + colMove
 
             if 0 <= newRow < 3 and 0 <= newCol < 3:
-                neighbor = list(self.state)
+                neighborState = list(self.state)
+
                 newZeroIndex = newRow * 3 + newCol
-                self.state[zeroIndex], self.state[newZeroIndex] = (
-                    self.state[newZeroIndex],
-                    self.state[zeroIndex],
+                neighborState[zeroIndex], neighborState[newZeroIndex] = (
+                    neighborState[newZeroIndex],
+                    neighborState[zeroIndex],
                 )
 
-                self.neighboringStates.append(neighbor)
+                neighbor = BoardState(state=neighborState)
+                self.neighbors.append(neighbor)
 
-    def getNeighboringStates(self):
-        return self.neighboringStates
+    def getNeighbors(self):
+        return self.neighbors
 
     def checkSolved(self):
         return True if self.state == BoardState.goalState else False
