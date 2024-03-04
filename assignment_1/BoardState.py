a3c7c1f52eda38142e22ff90f19079a5839ac06a
Ossama Tammam <ossamatammam02@gmail.com>
2024-03-04 23:58:41 +0200

    renaming for better code clarity

--- assignment_1/BoardState.py
+++ assignment_1/BoardState.py
@@ -2,45 +2,44 @@ import random
 
 
 class BoardState:
-    goalState = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    goal = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
-    def __init__(self, state: list = None, boardState: "BoardState" = None):
+    def __init__(self, layout: list = None, boardState: "BoardState" = None):
+        self.neighbors = []
+
         # check if it's already an object and clone it
         if boardState is not None:
-            self.state = boardState.state.copy()
+            self.layout = boardState.layout.copy()
             self.neighbors = boardState.neighbors.copy()
             return
 
-        # check if state is present
-        if state is not None:
-            self.state = state
+        # check if layout is present
+        if layout is not None:
+            self.layout = layout
 
-        # if state is not specified generate a random state
-        if state is None and boardState is None:
-            self.state = list(range(9))
-            random.shuffle(self.state)
-
-        # calculate the neighboring states for each board
-        self.neighbors = []
+        # if layout is not specified generate a random layout
+        if layout is None and boardState is None:
+            self.layout = list(range(9))
+            random.shuffle(self.layout)
 
     def calcNeighbors(self):
         possibleMoves = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # (row, col)
-        zeroIndex = self.state.index(0)
+        zeroIndex = self.layout.index(0)
         currRow, currCol = zeroIndex // 3, zeroIndex % 3
 
         for rowMove, colMove in possibleMoves:
             newRow, newCol = currRow + rowMove, currCol + colMove
 
             if 0 <= newRow < 3 and 0 <= newCol < 3:
-                neighborState = list(self.state)
+                neighborLayout = list(self.layout)
 
                 newZeroIndex = newRow * 3 + newCol
-                neighborState[zeroIndex], neighborState[newZeroIndex] = (
-                    neighborState[newZeroIndex],
-                    neighborState[zeroIndex],
+                neighborLayout[zeroIndex], neighborLayout[newZeroIndex] = (
+                    neighborLayout[newZeroIndex],
+                    neighborLayout[zeroIndex],
                 )
 
-                neighbor = BoardState(state=neighborState)
+                neighbor = BoardState(layout=neighborLayout)
                 self.neighbors.append(neighbor)
 
     def getNeighbors(self):
@@ -50,4 +49,4 @@ class BoardState:
         return self.neighbors
 
     def checkSolved(self):
-        return True if self.state == BoardState.goalState else False
+        return True if self.layout == BoardState.goal else False
